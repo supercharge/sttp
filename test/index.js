808b89e523b745bba61671f12ed14382f373d34a
Marcus Pöhls <mp@seda.digital>
2021-10-28 16:22:47 +0200

    use uvu and c8 instead of jest

--- test/index.js
+++ test/index.js
@@ -1,31 +1,33 @@
 'use strict'
 
+const { test } = require('uvu')
+const expect = require('expect')
 const { Sttp } = require('../dist')
 const Server = require('./test-server')
 
 const baseUrl = 'http://localhost:2020'
 
-describe('Sttp', () => {
-  beforeAll(async () => {
-    await Server.start()
-  })
-
-  afterAll(async () => {
-    await Server.stop()
-  })
-
-  it('sends a get request', async () => {
-    const response = await Sttp.get(baseUrl)
-    expect(response.status()).toEqual(200)
-    expect(response.payload()).toEqual('Success')
-  })
-
-  it('sends a get request with headers', async () => {
-    const response = await Sttp.withHeaders({
-      name: 'Marcus'
-    }).get(`${baseUrl}/with-headers`)
-
-    expect(response.status()).toEqual(200)
-    expect(response.payload()).toMatchObject({ name: 'Marcus' })
-  })
+test.before(async () => {
+  await Server.start()
 })
+
+test.after(async () => {
+  await Server.stop()
+})
+
+test('sends a get request', async () => {
+  const response = await Sttp.get(baseUrl)
+  expect(response.status()).toEqual(200)
+  expect(response.payload()).toEqual('Success')
+})
+
+test('sends a get request with headers', async () => {
+  const response = await Sttp.withHeaders({
+    name: 'Marcus'
+  }).get(`${baseUrl}/with-headers`)
+
+  expect(response.status()).toEqual(200)
+  expect(response.payload()).toMatchObject({ name: 'Marcus' })
+})
+
+test.run()
