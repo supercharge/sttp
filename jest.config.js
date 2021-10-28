8084ebd5623f6390a05ac62b8ca84f14a4bf2c9c
Marcus Pöhls <mp@seda.digital>
2021-10-28 16:22:53 +0200

    remove jest

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-module.exports = {
-  collectCoverage: true,
-  testEnvironment: 'node',
-  coverageReporters: ['text', 'html'],
-  testMatch: ['**/test/**/*.[jt]s?(x)'],
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/test/test-server.js']
-}
