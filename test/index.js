3d2c4e9dd6649907bb0d8937c7cd18b34246b7f8
Marcus Pöhls <mp@seda.digital>
2021-11-01 13:58:22 +0100

    add tests

--- test/index.js
+++ test/index.js
@@ -12,7 +12,7 @@ test.before(async () => {
   server = Server.create().withMiddleware(ctx => {
     ctx.response.body = {
       headers: ctx.request.headers,
-      payload: ctx.body,
+      payload: ctx.request.body,
       query: ctx.request.query
     }
   })
@@ -30,12 +30,62 @@ test('sends a get request', async () => {
 })
 
 test('sends a get request with headers', async () => {
-  const response = await Sttp.withHeaders({
-    name: 'Marcus'
-  }).get(`${baseUrl}/with-headers`)
+  const response = await Sttp
+    .withHeaders({ name: 'Supercharge' })
+    .get(`${baseUrl}/with-headers`)
 
   expect(response.status()).toEqual(200)
-  expect(response.payload()).toMatchObject({ headers: { name: 'Marcus' } })
+  expect(response.payload()).toMatchObject({ headers: { name: 'Supercharge' } })
+})
+
+test('Sttp.withQueryParams()', async () => {
+  const response = await Sttp
+    .withQueryParams({ name: 'Supercharge' })
+    .get(`${baseUrl}/with-query-params`)
+
+  expect(response.status()).toEqual(200)
+  expect(response.payload()).toMatchObject({ query: { name: 'Supercharge' } })
+})
+
+test('Sttp.withToken()', async () => {
+  const response = await Sttp
+    .withToken('token')
+    .get(`${baseUrl}/with-query-params`)
+
+  expect(response.status()).toEqual(200)
+  expect(response.payload()).toMatchObject({
+    headers: { authorization: 'Bearer token' }
+  })
+})
+
+test('Sttp.withToken() and type', async () => {
+  const response = await Sttp
+    .withToken('token', 'X-Bearer')
+    .get(`${baseUrl}/with-query-params`)
+
+  expect(response.status()).toEqual(200)
+  expect(response.payload()).toMatchObject({
+    headers: { authorization: 'X-Bearer token' }
+  })
+})
+
+test('supports fluent interface', async () => {
+  const response = await Sttp
+    .withQueryParams({ id: 1 })
+    .withHeaders({ 'x-auth': 'secret' })
+    .withPayload({ name: 'Supercharge' })
+    .withToken('bearer-token')
+    .post(`${baseUrl}/fluent-interface`)
+
+  expect(response.status()).toEqual(200)
+  expect(response.payload()).toMatchObject({
+    query: { id: '1' },
+    payload: { name: 'Supercharge' },
+    headers: {
+      'x-auth': 'secret',
+      authorization: 'Bearer bearer-token'
+    }
+  })
 })
 
 test.run()
